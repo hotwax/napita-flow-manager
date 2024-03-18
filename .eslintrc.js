579d99fe72da57e49b49bf4b14cd3607cecac091
Ritika-Patel08 <ritikapatel8870@gmail.com>
2024-03-18 15:02:41 +0530

    Improved: code by removing unwanted flows and files

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,6 @@ module.exports = {
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
       env: {
         jest: true
       }
