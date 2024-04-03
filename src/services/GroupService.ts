0c27e29295ab224766397fa8ed5ee09bf04de8d0
Ritika-Patel08 <ritikapatel8870@gmail.com>
2024-04-03 10:49:43 +0530

    Improved: code by removing commented code

--- src/services/GroupService.ts
+++ src/services/GroupService.ts
@@ -1,6 +1,5 @@
 import { useUserStore } from '@/store/user'
-import { useGroupStore } from '@/store/groups'
-import api, { client } from "@/api"
+import api from "@/api"
 
 const FetchProcessGroups = async (root: any): Promise<any> => {
   const userStore = useUserStore();
@@ -29,38 +28,6 @@ const FetchProcessGroups = async (root: any): Promise<any> => {
     return Promise.reject("Failed to fetch process groups");
   }
 }
-
-// const FetchProcessGroupsConnection = async (root: any, token: any, groupId: any): Promise<any> => {
-//   const userStore = useUserStore();
-//   const groupStore = useGroupStore()
-//   const baseURL = userStore.getBaseUrl;
-//   const id = groupStore.getCurrentProcessGroup
-//   try {
-//     const resp = await client({
-//       url: `flow/process-groups/${id}`,
-//       method: "get",
-//       baseURL,
-//       headers: {
-//         'Authorization': `Bearer ${token}`
-//       }
-//     }) as any;
-//     const connections = resp.data.processGroupFlow.flow.connections.map((group: any) => ({
-//        sourceId: group.component.source.groupId,
-//        destinationId: group.component.destination.groupId
-//     }));  
-//     // Find the connection with destinationId equal to groupId
-//     const findConnection = connections.find((connection: any) => connection.destinationId === groupId);
-
-//     if (findConnection) {
-//       return Promise.resolve({ sourceId: findConnection.sourceId, destinationId: findConnection.destinationId });
-//     } else {
-//       return Promise.reject("Connection not found for groupId: " + groupId);
-//     }
-//   } catch (err) {
-//     console.log(err);  
-//     return Promise.reject("Failed to fetch process groups connections");
-//   }
-// }
 
 const FetchProcessByGroups = async (root: any): Promise<any> => {
   const userStore = useUserStore();
